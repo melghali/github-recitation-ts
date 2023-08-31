8819d6d4b46496fd48766c5db9a6aa1e35a7e810
Mohamad El Ghali <melghali@andrew.cmu.edu>
2023-08-30 21:45:45 -0400

    Working on data types in fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,12 +1,12 @@
 // Endpoint for querying the fibonacci numbers
 import fibonacci from "./fib";
 
-export default (req, res) => {
+export default (req: any, res: any) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  let result: string = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
     result = `fibonacci(${num}) is undefined`;
